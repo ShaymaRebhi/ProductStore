b092413d823bb09a3f670d0c520a7007519d5c1f
ShaymaREBHI <shayma.rebhi@esprit.tn>
2022-02-27 21:56:05 +0100

    Entity Framework Core

---
+++ PS.Services/StringExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PS.Services
+{
+    public static class StringExtension
+    {
+        public static string Upper(this string s)
+        {
+            s = s[0].ToString().ToUpper()+s.Substring(1,s.Length-1);
+            return s;
+        }
+    }
+}
